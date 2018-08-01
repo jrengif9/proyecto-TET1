c742f778b5756dabf34417723ed477d18f8fd022
Jose Rengifo <jrengif9@eafit.edu.co>
2018-08-01 09:33:42 -0500

    mapa funcional en el ambiente dev

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'position/getPosition', as: 'get_Position'
+  get 'position/save'
+  get 'position/show'
   devise_for :users
+
+  root 'position#getPosition'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
