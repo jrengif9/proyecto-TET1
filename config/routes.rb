f005565fb08c564ca164146be7b2652b9b0ca468
Jose Rengifo <jrengif9@eafit.edu.co>
2018-08-03 17:58:32 -0500

    finalizando interfaz

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'position/show'
   devise_for :users
 
+  
   root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
